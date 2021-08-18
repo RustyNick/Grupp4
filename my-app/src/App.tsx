a9e3953e42ef1251f4d50cbafdee3378a5f5f884
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2021-08-18 14:02:00 +0200

    ändringar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { TouchBackend } from 'react-dnd-touch-backend'
+import { DndProvider } from 'react-dnd'
 import './App.css';
 import Header from "./components/Header"
+
 
 function App() {
   return (
     <div className="App">
-
+      <h1>KA-NI-VI</h1>
       <Header />
 
     </div>
