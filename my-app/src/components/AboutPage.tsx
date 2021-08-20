9790cb32a38c3db1426c97d50f9fc3c3adfbad6e
Kanan <kenan@luxar.se>
2021-08-20 15:16:35 +0200

    React Router
    nav bar i header

---
+++ my-app/src/components/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+export const AboutPage: React.FC = () => {
+    const history = useHistory ()
+    return(
+        <>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, modi!</p>
+        <button className="btn" onClick={()=>history.push('/')}>Back to START</button>
+        </>
+    )
+}
---
+++ my-app/src/components/Group4.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+<h1>banan3</h1>
+
+const Group4 = () => {
+    return (
+        <div className="header">
+            hi
+           
+        </div>
+    )
+}
+
+export default Group4
